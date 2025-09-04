36370d9469ed522dfa7adbefbbd87ba3645ce823
kamipalomino <cpalomino@frba.utn.edu.ar>
2025-09-03 22:29:52 -0300

    f8nal completi

--- src/Finales/2024-12-07.pl
+++ src/Finales/2024-12-07.pl
@@ -6,7 +6,7 @@ jugador(maria, [anio(2015)]).
 
 %videoJuego(Titulo, Desarrolladora, [Generos], Lanzamiento).
 videoJuego(tetris, nintendo, [casual, tirador], 1989).
-videoJuego(sims, rock, [plataforma], 2010).
+videoJuego(sims, eA, [simulacion], 2010).
 videoJuego(candy, android, [casual], 2017).
 videoJuego(teg, nintendo, [mundoabierto], 2017).
 videoJuego(solitario, rock, [plataforma], 2017).
@@ -27,6 +27,13 @@ cumpleUnaPreferencia(anio(A), Titulo) :-
     videoJuego(Titulo, _, _, Lanzamiento),
     Lanzamiento >= A.
     
+ comite(Titulo):-
+     videoJuego(Titulo, _, _, _),
+    forall(jugador(J, _), cumplePreferencia(J, Titulo)).
+    
 preferidosDelComite(Titulos) :-
+    findall(Titulo, comite(Titulo) , Titulos).
+ 
+ovejaNegra(Titulo) :-
     videoJuego(Titulo, _, _, _),
-    findall(Titulo, forall(jugador(J, _), cumplePreferencia(J, Titulo)), Titulos).
+    not(cumplePreferencia(_,Titulo)).
