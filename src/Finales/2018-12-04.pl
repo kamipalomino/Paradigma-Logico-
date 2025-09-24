23a2def2d8fdf4d54b3c5154ac30b1462f7a1ff7
kamipalomino <cpalomino@frba.utn.edu.ar>
2025-09-24 00:44:04 -0300

    final 2018

---
+++ src/Finales/2018-12-04.pl
@@ -0,0 +1,17 @@
+% acuerdo(P1, P2, Algo).
+
+acuerdo(argentina, china, trigo).
+acuerdo(argentina, rusia, trigo).
+acuerdo(eeuu, argentina, carne).
+acuerdo(china, rusia, arroz).
+acuerdo(argentina, francia, carne).
+acuerdo(argentina, brasil, carne).
+acuerdo(francia, arabia, carne).
+acuerdo(brasil, zimbague, carne).
+acuerdo(arabia, chile, carne).
+
+recibe(Pais, Pais2):- 
+    acuerdo(Pais,_, M),
+    acuerdo(_,Pais2,M), 
+    acuerdo(_,_, M).
+    
